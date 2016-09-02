805131d881ad5af27809a4050a108b1c7a009d50
Heejong Ahn <heejongahn@gmail.com>
2016-09-02 20:22:20 +0900

    Add basic inquirer template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log(1)
+import inquirer from 'inquirer'
+
+const questions = [
+  {
+    type: 'confirm',
+    name: 'trivial',
+    message: 'Do you think Heejong is awesome?',
+    default: true
+  }
+]
+
+inquirer
+  .prompt(questions)
+  .then(answer => {
+    if (answer.trivial) {
+      console.log('That\'s right! Great job done there!')
+    } else {
+      console.log('You\'re stupid.')
+    }
+  })
