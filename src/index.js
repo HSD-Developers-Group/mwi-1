5a25b611b34c9bf2adef465ad087428890ef197c
Heejong Ahn <heejongahn@gmail.com>
2016-09-04 17:12:27 +0900

    Refine inquirer questioning logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import inquirer from 'inquirer'
 import * as path from 'path'
-import * as fs from 'fs'
+import * as fs from 'mz/fs'
 
 const questions = [
   {
@@ -9,59 +9,80 @@ const questions = [
     default: 'coolApp'
   },
   {
+    name: 'description',
+    message: 'A brief description, please.',
+    default: 'An application cooler than DJ Kool Herc'
+  },
+  {
     type: 'list',
     name: 'spa',
     message: 'Do you have any specific SPA library in your mind?',
     choices: [
-      'ReactJS',
-      'AngularJS 1.x',
-      'AngularJS 2',
-      'No, thanks.'
+      'react',
+      'angular',
+      'angular2',
+      'Nothing'
     ]
   },
   {
-    type: 'confirm',
-    name: 'redux',
-    message: 'With redux?',
-    when: (answer) => { return answer.spa == 'ReactJS' }
+    type: 'checkbox',
+    name: 'additional',
+    message: 'I suppose some of these might come handy with your choice..',
+    choices: answer => {
+      const base = ['jquery', 'whatwg-fetch', 'eslint']
+      switch (answer.spa) {
+        case 'react':
+          return base.concat(['redux', 'react-router'])
+        case 'angular':
+        case 'angular2':
+          return base.concat(['angular-material'])
+        default:
+          return base
+      }
+    }
   },
   {
     type: 'list',
     name: 'frontend',
     message: 'Cool. For the frontend framework?',
     choices: [
-      'Twitter bootstrap',
-      'Semantic-UI',
-      'No, thanks.'
+      'bootstrap',
+      'semantic-ui',
+      'Nothing'
     ]
   },
   {
-    type: 'confirm',
-    name: 'jquery',
-    message: 'And do you need old pal jQuery too?'
-  },
-  {
     type: 'list',
-    name: 'build tool',
+    name: 'buildTool',
     message: 'Last but not least, your build/bundling tool?',
     choices: [
       'webpack',
       'rollup',
-      'gulp'
+      'gulp',
+      'Nothing'
     ]
   }
 ]
 
+const parseAnswer = (answer) => {
+  const { name, spa, additional, frontend, buildTool } = answer
+  const packages = [spa, frontend, buildTool]
+    .filter(item => item !== 'Nothing')
+    .concat(additional)
+
+  return { name, packages }
+}
+
 export default () => {
+  const cwd = process.cwd()
+
   inquirer
   .prompt(questions)
   .then(answer => {
-    fs.mkdir(path.join(process.cwd(), answer.name), (err) => {
-      if (err) {
-        console.log(err.message)
-      } else {
-        Object.keys(answer).map(key => console.log(`${key}: ${answer[key]}`))
-      }
-    })
+    const packageInfo = parseAnswer(answer)
+
+    fs.mkdirSync(path.join(cwd, packageInfo.name))
+    console.log(packageInfo.packages)
   })
+  .catch(err => console.log(err.message))
 }
